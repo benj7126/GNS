6206793997dbe0c053fba40f4c815b6c5e3782c0
catNull <benvb2004@gmail.com>
2023-11-10 13:53:26 +0100

    Semi working simple text field

--- src/include/Elements/SimpleTextField.h
+++ src/include/Elements/SimpleTextField.h
@@ -7,11 +7,24 @@
 namespace Elements { // managing note types
 	class SimpleTextField : public Element {
 	protected:
-		std::string text;
+		std::string text = "";
+		int cursorPosition = 0;
+
+		int fontSize = 16;
+		std::string fontType = "";
+
+		int textLineSpacing = 15;
+		int spacing = 0;
+
+		Color textColor = BLACK;
+		Color backgroundColor = WHITE;
 
 	public:
 		void Draw(Vector2 offset) override;
 		void Update() override;
 		void MousePressed(Vector2 position) override;
+
+		int GetCursorIndex(Vector2 localMousePosition);
+		void CustomTextDraw(Vector2 origin);
 	};
 }
--- src/src/Elements/SimpleTextField.cpp
+++ src/src/Elements/SimpleTextField.cpp
@@ -6,12 +6,110 @@
 
 void Elements::SimpleTextField::Draw(Vector2 offset) {
 	// std::cout << offset.x + position.x << offset.y + position.y << size.x << size.y << std::endl;
-	int fontSize = 12;
-	Font font = FontManager::GetFont("", fontSize);
+	// std::cout << textFont << std::endl;
+	Font font = FontManager::GetFont(fontType, fontSize);
 	
-	DrawRectangle(offset.x + position.x, offset.y + position.y, size.x, size.y, ORANGE);
+	DrawRectangle(offset.x + position.x, offset.y + position.y, size.x, size.y, ORANGE); // size is {0, 0}...
+	
+	CustomTextDraw({offset.x + position.x, offset.y + position.y});
+}
 
-	DrawTextEx(font, text.c_str(), {offset.x + position.x, offset.y + position.y}, fontSize, 0, BLACK);
+void Elements::SimpleTextField::CustomTextDraw(Vector2 origin){
+	Font font = FontManager::GetFont(fontType, fontSize);
+	const char* cText = text.c_str();
+	
+	// basically just raylibs code, lol
+    int size = TextLength(cText);    // Total size in bytes of the text, scanned by codepoints in loop
+
+	int curIndex = 0;
+
+    int textOffsetY = 0;            // Offset between lines (on linebreak '\n')
+    float textOffsetX = 0.0f;       // Offset X to next character to draw
+
+    for (int i = 0; i < size;)
+    {
+        // Get next codepoint from byte string and glyph index in font
+        int codepointByteCount = 0;
+        int codepoint = GetCodepointNext(&text[i], &codepointByteCount);
+        int index = GetGlyphIndex(font, codepoint);
+
+        if (codepoint == '\n')
+        {
+            // NOTE: Line spacing is a global variable, use SetTextLineSpacing() to setup
+            textOffsetY += textLineSpacing;
+            textOffsetX = 0.0f;
+        }
+        else
+        {
+            if ((codepoint != ' ') && (codepoint != '\t'))
+            {
+                DrawTextCodepoint(font, codepoint, { origin.x + textOffsetX, origin.y + textOffsetY }, fontSize, BLACK);
+            }
+
+			if (curIndex == cursorPosition)
+				DrawRectangle(origin.x + textOffsetX, origin.y + textOffsetY, 1, fontSize, BLACK);
+
+			curIndex++; // idk if this should be here or before, but whatever...
+
+            if (font.glyphs[index].advanceX == 0) textOffsetX += ((float)font.recs[index].width + spacing);
+            else textOffsetX += ((float)font.glyphs[index].advanceX + spacing);
+        }
+
+        i += codepointByteCount;   // Move text bytes counter to next codepoint
+    }
+
+	if (curIndex == cursorPosition)
+		DrawRectangle(origin.x + textOffsetX, origin.y + textOffsetY, 1, fontSize, BLACK);
+}
+
+int Elements::SimpleTextField::GetCursorIndex(Vector2 localMousePosition){
+	Font font = FontManager::GetFont(fontType, fontSize);
+	const char* cText = text.c_str();
+
+	// basically just raylibs code, lol
+    int size = TextLength(cText);    // Total size in bytes of the text, scanned by codepoints in loop
+
+	int curIndex = 0;
+
+    int textOffsetY = 0;            // Offset between lines (on linebreak '\n')
+    float textOffsetX = 0.0f;       // Offset X to next character to draw
+    for (int i = 0; i < size;)
+    {
+        // Get next codepoint from byte string and glyph index in font
+        int codepointByteCount = 0;
+        int codepoint = GetCodepointNext(&cText[i], &codepointByteCount);
+        int index = GetGlyphIndex(font, codepoint);
+
+        if (codepoint == '\n')
+        {
+            // NOTE: Line spacing is a global variable, use SetTextLineSpacing() to setup
+            textOffsetY += textLineSpacing;
+            textOffsetX = 0.0f;
+        }
+        else
+        {
+			float xToAdd = 0;
+
+            if (font.glyphs[index].advanceX == 0) xToAdd = ((float)font.recs[index].width + spacing);
+            else xToAdd = ((float)font.glyphs[index].advanceX + spacing);
+
+            if ((codepoint != ' ') && (codepoint != '\t'))
+            {
+				Vector2 charPosition = { textOffsetX, textOffsetY };
+				if (charPosition.x + xToAdd > localMousePosition.x && charPosition.y < localMousePosition.y && charPosition.y + fontSize > localMousePosition.y)
+					return curIndex;
+
+				curIndex++; // idk if this should be here or before, but whatever...
+            }
+
+			textOffsetX += xToAdd;
+			
+        }
+
+        i += codepointByteCount;   // Move text bytes counter to next codepoint
+    }
+
+	return text.size(); // or -1 idk yet
 }
 
 void Elements::SimpleTextField::Update(){
@@ -22,7 +120,8 @@ void Elements::SimpleTextField::Update(){
 		{
 			if ((key >= 32) && (key <= 125))
 			{
-				text += (char)key;
+				text.insert(text.begin()+cursorPosition, (char)key);
+				cursorPosition++;
 			}
 
 			key = GetCharPressed();
@@ -30,14 +129,31 @@ void Elements::SimpleTextField::Update(){
 
 		if (IsKeyPressed(KEY_BACKSPACE))
 		{
-			if (!text.empty())
-				text.pop_back();
+			if (cursorPosition != 0){
+				text.erase(text.begin() + cursorPosition-1);
+				cursorPosition--;
+			}
 		}
+
+		if (IsKeyPressed(KEY_DELETE))
+		{
+			if (cursorPosition != text.size()){
+				text.erase(text.begin() + cursorPosition);
+				cursorPosition;
+			}
+		}
+
+		if (IsKeyPressed(KEY_LEFT))
+			cursorPosition = cursorPosition == 0 ? 0 : cursorPosition-1;
+
+		if (IsKeyPressed(KEY_RIGHT))
+			cursorPosition = cursorPosition == text.size() ? text.size() : cursorPosition+1;
 	}
 }
 
 void Elements::SimpleTextField::MousePressed(Vector2 pos) {
-	std::cout << "test" << std::endl;
+	std::cout << "test char pos thingy: " << GetCursorIndex(pos) << std::endl;
+	cursorPosition = GetCursorIndex(pos);
 	selectedElement = this;
 	// TextInputHandler::LinkForInput(&text);
 }
